693178eea9afe356b5540910077c2b961dccce88
Kenyon Earls <kenter330@gmail.com>
2025-06-19 22:43:48 -0400

    update a

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const fs = require("fs");
 const path = require('path');
 const process = require('process');
 const { google } = require('googleapis');
+const { console } = require("inspector");
 
 const CREDENTIALS_PATH = path.join(process.cwd(), 'json/credentials.json');
-const range = 'June';
+let range;
 
 const auth = new google.auth.GoogleAuth({
     keyFile: CREDENTIALS_PATH,
@@ -40,7 +41,7 @@ function getID(DocType) {
 async function pull(DocType, RecordDateFrom, RecordDateTo) {
     const options = new Chrome.Options()
         .excludeSwitches('enable-logging')
-        .addArguments("--profile-directory=Default", "--disable-extensions", "--disable-dev-shm-usage", "--no-sandbox")
+        .addArguments("--disable-extensions", "--disable-dev-shm-usage", "--no-sandbox")
         .setPageLoadStrategy("eager")
         .setUserPreferences({
             "download.default_directory": `${__dirname}\\csv\\`,
@@ -66,7 +67,7 @@ async function pull(DocType, RecordDateFrom, RecordDateTo) {
             await driver.findElement(By.id("RecordDateTo")).clear();
             await driver.findElement(By.id("RecordDateTo")).sendKeys(RecordDateTo);
 
-            await driver.findElement(By.id("btnSearch")).submit();
+            await driver.findElement(By.id("btnSearch")).click();
 
             await driver.wait(until.elementLocated(By.id("btnCsvButton")), 5000);
             await driver.findElement(By.id("btnCsvButton")).click();
@@ -138,11 +139,76 @@ async function openLink(driver, id, index, name, namelist, DocType) {
     return index;
 }
 
+async function pullProbates(RecordDateFrom, RecordDateTo) {
+    const options = new Chrome.Options()
+        .excludeSwitches('enable-logging')
+        .addArguments("--disable-extensions", "--disable-dev-shm-usage", "--no-sandbox")
+        .setPageLoadStrategy("eager")
+        .setUserPreferences({
+            "download.default_directory": `${__dirname}\\csv\\`,
+            "download.prompt_for_download": false
+        });
+
+    let driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build();
+    await driver.get("https://courtrecords.mypinellasclerk.gov/");
+    await driver.wait(until.elementLocated(By.css("#categoryPicker > div > div > button")), 1000);
+    await driver.findElement(By.css("#categoryPicker > div > div > button")).click();
+    await driver.findElement(By.css("#categoryPicker > div > div > ul > li.multiselect-item.multiselect-all.active > a > label")).click();
+    await driver.findElement(By.css("#categoryPicker > div > div > ul > li:nth-child(18) > a > label")).click();
+    await driver.findElement(By.css("#categoryPicker > div > div > button")).click();
+    await driver.findElement(By.id("DateFrom")).click();
+    await driver.findElement(By.id("DateFrom")).sendKeys(RecordDateFrom);
+    await driver.findElement(By.id("DateTo")).click();
+    await driver.findElement(By.id("DateTo")).sendKeys(RecordDateTo);
+    await driver.findElement(By.id("caseSearch")).click();
+
+    let index = 0;
+    await driver.wait(until.elementLocated(By.css("#caseList")), 10000);
+    let len = parseInt((await driver.findElement(By.css("#main > div:nth-child(10) > div > div.card-header > div > div.col-sm-8.col-md-8.search-bar-results")).getText()).split(" ")[2]);
+    let people = [];
+    while (index < len) {
+        await driver.wait(until.elementLocated(By.css("#caseList_length")), 10000);
+        await driver.findElement(By.css("#caseList_length > label > select")).click();
+        await driver.findElement(By.css("#caseList_length > label > select > option:nth-child(5)")).click();
+        await driver.findElement(By.css("#caseList_length > label > select")).click();
+        await driver.sleep(10);
+        index++;
+
+        let name = (await driver.findElement(By.css(`#caseList > tbody > tr:nth-child(${index}) > td:nth-child(4)`)).getText()).split(":")[1].replace("THE ESTATE OF", "").trim();  
+        if (people.map((person) => person[0]).includes(name)) continue;
+
+        await driver.findElement(By.css(`#caseList > tbody > tr:nth-child(${index}) > td.colCaseNumber > a`)).click();
+        let beneficiary = await driver.findElement(By.css("#partiesCollapse > div > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > span")).getText();
+        let attorney = await driver.findElement(By.css("#partiesCollapse > div > div > table > tbody > tr:nth-child(2) > td:nth-child(3)")).getText();
+        await driver.findElement(By.css("#caseDetails > div.card-header > div > div.col-sm-2.col-md-2.search-bar-refine > a.pull-left.print-icon.print-icon-text")).click();
+        people.push([name, beneficiary, attorney]);
+    }
+    
+    for (let person of people) {
+        let name = person[1]
+        if (name.split(" ").length <= 1) continue;
+        
+        let namelist = name.split(" ");
+        if (namelist.length > 3 && !(namelist.includes("JR") || namelist.includes("SR") || namelist.includes("TRE")) || ["LLC", "INC", "TRUSTEE"].includes(namelist[namelist.length - 1])) continue;
+
+        await driver.get(`https://www.pcpao.gov/quick-search?qu=1&input=${name.split(" ")[0]},%20${name.split(" ")[1]}&search_option=owner`);
+
+        let properties = [];
+        for (let row of (await driver.findElements(By.css("tr[role=row]")))) {
+            let data = await row.findElements(By.css("td"));
+            if (data.length > 1 && ["0000", "0090", "0110", "0310", "0311", "0810", "0820", "0822", "1000", "1032", "1090", "1120", "1135", "1423", "2226", "2816", "3912", "3913", "4000", "4090"].includes((await data.at(5).getText()).trim().split(" ")[0])) properties.push(await data.at(2).getText());
+        }
+        console.log(properties);
+    }
+}
+
 async function run() {
-    await pull("PROBATE DOCUMENT",          "05/1/2025", "05/31/2025");
-    await pull("LIENS",                     "05/1/2025", "05/31/2025");
-    await pull("LIS PENDENS",               "05/1/2025", "05/31/2025");
-    await pull("NOTICE OF CONTEST OF LIEN", "05/1/2025", "05/31/2025");
+    range = 'July';
+    await pullProbates("06/01/2025", "06/08/2025");
+    //await pull("PROBATE DOCUMENT",          "03/1/2025", "03/31/2025");
+    //await pull("LIENS",                     "03/1/2025", "03/15/2025");
+    //await pull("LIS PENDENS",               "03/1/2025", "03/31/2025");
+    //await pull("NOTICE OF CONTEST OF LIEN", "03/1/2025", "03/31/2025");
 }
 
 run();
