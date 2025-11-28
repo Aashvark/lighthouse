92b4697a3e33e6365a0242ac10d7bc5199e9078e
Keńien Earls <kenter330@gmail.com>
2025-11-27 21:46:23 -0500

    update

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ fastify.register(require("@fastify/formbody"));
 handlebars.logger.level = "debug";
 
 fastify.get("/",  function (request, reply) { return reply.view('/src/index.hbs'); });
-fastify.post("/", function (request, reply) { return reply.view("/src/index.hbs", { text: "works" }); });
+fastify.post("/", function (request, reply) {
+  let month = new Date(request.body.start).toLocaleString('default', { month: 'long' });
+  return reply.view("/src/index.hbs", { text: month });
+});
 
 fastify.listen({ port: process.env.PORT || 4000, host: "0.0.0.0" }, function (err, address) { if (err) { console.error(err); process.exit(1); } } );
